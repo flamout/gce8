faf28e3afd73f57ea57727f0a6bea5ddeaebfca4
flamout <florent.mouton@free.fr>
2026-05-07 16:00:02 +0200

    modified:   core/class/gce8.class.php

--- core/class/gce8.class.php
+++ core/class/gce8.class.php
@@ -264,6 +264,7 @@ class gce8 extends eqLogic {
 		if ($action=="imp") {
 			if ($nbrelais=="8") {
 				$mess='echo RLY'.$num_relais.'1 >'.$port_carte; 
+        Log::add ('gce8', 'info', $mess)
 			}
 			else {
 				$mess='echo S'.$num_relais.'1 >'.$port_carte;
