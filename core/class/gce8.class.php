fe631fb2f58b436fd4db09eb190e2829a97f8883
flamout <florent.mouton@free.fr>
2026-05-07 10:38:12 +0200

    modified:   core/class/gce8.class.php

--- core/class/gce8.class.php
+++ core/class/gce8.class.php
@@ -347,7 +347,7 @@ class gce8 extends eqLogic {
         }
     $nmrel1 = $nmrel1.' ON';
        log::add ('gce8','info',$nmrel1);
-    $gce8cmd->setName(__($nmrel1,__FILE__));
+    $gce8cmd->setName(__($nmrel1, __FILE__));
 		$gce8cmd->setLogicalId('r1on');
 	  $gce8cmd->setEqLogic_id($this->getId());
 		$gce8cmd->setType('action');
